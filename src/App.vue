54a7f5d1316615cb53ca0facf0fb0c56179b2258
devjuw <devjuwelier@mail.com>
2024-01-29 22:42:09 +0100

    ending landing page

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,22 @@
 </script>
 
 <template>
-
+  <div class="container-md" style="margin-top: 80px;">
+    <div class="row">
+      <div class="col-sm-6 text-center text-sm-start">
+        <img src="./assets/a-juwelier.jpg" class="img-fluid" alt="a-juwelier">
+      </div>
+      <div class="col-sm-6 text-center text-sm-start">
+        <h1 style="margin-top: 90px;" class="my-font-page">A Juwelier</h1>
+        <a href="https://ajuwelier.de/" class="btn btn-outline-info btn-lg mt-5"><span class="d-inline-block me-1 my-font-page">Enter</span> <i class="fa-solid fa-hand-pointer"></i></a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
+
+   .my-font-page {
+    font-family: 'Courier New', Courier, monospace;
+   }
 </style>
